7ce3a5033da3986bd3474816b0b6e450e6a6c42a
mregrock <mregrock@yandex.ru>
2024-02-10 11:22:19 +0300

    Test operator <<

--- main.cpp
+++ main.cpp
@@ -2,9 +2,11 @@
 #include "BigInteger.h"
 
 int main() {
-    BigInteger a = 899123499123999_bi;
-    BigInteger b = 151231239123123_bi;
+    BigInteger a = 894239429349_bi;
+    BigInteger b = 9931293999999999_bi;
     BigInteger c = a * b;
-    std::cout << c.GetChunk(0) << " " << c.GetChunk(1) << " " << c.GetChunk(2) << " " << c.GetChunk(3);
+    std::cout << c.GetChunk(0) << " " << c.GetChunk(1) << " " << c.GetChunk(2) << " " << c.GetChunk(3) << "\n";
+    std::cout << c.GetSizeInChunks() << "\n";
+    std::cout << c;
     return 0;
 }
